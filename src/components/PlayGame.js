7c625b4fd4a494088775fc3657647fe54b4071ef
jcuhnoio <junochoi2003@gmail.com>
2024-04-26 22:04:37 -0400

    Can now type along in the playgame state

--- src/components/PlayGame.js
+++ src/components/PlayGame.js
@@ -15,7 +15,7 @@ const PlayGame = ({ onChangeScore }) => {
                 const position = Math.floor(Math.random() * wordList.length);
                 dataTypingTest.push({
                     value: wordList[position],
-                    status: null,
+                    status: [],
                 });
             }
             setDataTyping(dataTypingTest);
@@ -32,19 +32,31 @@ const PlayGame = ({ onChangeScore }) => {
                 ...textTyping,
                 value: valueInput
             });
-        } else if (textTyping.value !== "") {
-            checkResult();
+        } else {
+            setTextTyping({
+                value: "",
+                position: textTyping.position + 1
+            })
         }
+        console.log(textTyping)
+        checkResult(valueInput);
     };
 
-    const checkResult = () => {
+    const checkResult = (valueInput) => {
         const dataCheck = dataTyping;
         const wordCheck = dataCheck[textTyping.position].value;
-        if (textTyping.value === wordCheck) {
-            dataCheck[textTyping.position].status = true;
+        if (valueInput.at(-1) === wordCheck[valueInput.length - 1]) {
+            dataCheck[textTyping.position].status.push('correct');
         } else {
-            dataCheck[textTyping.position].status = false;
+            dataCheck[textTyping.position].status.push('incorrect');
         }
+        // for (let i = 0; i < wordCheck.length; i++) {
+        //     if (textTyping.value[i] === wordCheck[i]) {
+        //         dataCheck[textTyping.position].status.push('correct');
+        //     } else {
+        //         dataCheck[textTyping.position].status.push('incorrect');
+        //     }
+        // }
         setDataTyping(dataCheck);
     };
 
@@ -55,15 +67,19 @@ const PlayGame = ({ onChangeScore }) => {
                     {dataTyping.map((word, index) => (
                         <div className="word" key={index}>
                             {word.value.split("").map((char, charIndex) => (
-                                <letter key={charIndex}>{char}</letter>
+                                <letter className={dataTyping[index].status[charIndex]} key={charIndex}>{char}</letter>
                             ))}
                         </div>
                     ))}
                 </div>
             </div>
             <div className="inputForm">
-                <input type="text" value={textTyping.value} 
-                onChange={handleChangeTyping}/>
+                <input
+                    type="text"
+                    value={textTyping.value}
+                    onChange={handleChangeTyping}
+                    autoFocus
+                />
             </div>
         </div>
     );
