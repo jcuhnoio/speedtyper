a11b1d648ac6019044beefcc40d7cdcb2db94624
ben k <benkim128@gmail.com>
2024-05-01 16:47:38 -0400

    deploy production

---
+++ build/precache-manifest.6483a28e4da3a7364a0c90fe69ba2ef6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3fd8f65cf1621df763cd00faceb810b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a87ef32d4826eef5b404",
+    "url": "./static/css/main.f2f81019.chunk.css"
+  },
+  {
+    "revision": "6699f2559d7ab8601410",
+    "url": "./static/js/2.b49763a4.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "./static/js/2.b49763a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0749de172abdbaa0bb3c",
+    "url": "./static/js/3.abc3056c.chunk.js"
+  },
+  {
+    "revision": "a87ef32d4826eef5b404",
+    "url": "./static/js/main.d4e4a4e6.chunk.js"
+  },
+  {
+    "revision": "2821d60adca2b0f6c38b",
+    "url": "./static/js/runtime-main.669b5292.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "./static/media/logo.06e73328.svg"
+  }
+]);
